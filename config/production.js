f97c616ab750f550e56e3c6bdbb031dd2deb24c6
Hassan Bouzlima <bouzlima.h@live.fr>
2023-08-09 13:26:41 +0100

    Add dbName attribute to production config

--- config/production.js
+++ config/production.js
@@ -1,6 +1,7 @@
 module.exports = {
   port: process.env.PORT,
   dbUri: process.env.dbUri,
+  dbName: process.env.dbName,
   frontAppVerif: process.env.frontAppVerif,
   jwtKey: process.env.jwtKey,
 };
--- utils/db.js
+++ utils/db.js
@@ -2,8 +2,12 @@ const config = require("../config");
 const mongoose = require("mongoose");
 
 module.exports = (dbUri = config.dbUri) => {
-  return mongoose.connect(dbUri, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  });
+  return mongoose.connect(
+    dbUri,
+    { dbName: config.dbName },
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }
+  );
 };
