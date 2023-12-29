7f0b9eba6ec849f5d8c7f2f92f9a8dff2a002953
Hassan Bouzlima <bouzlima.h@live.fr>
2023-12-29 19:57:32 +0100

    Update dbUri for dev environment

--- config/development.js
+++ config/development.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: 3000,
-  dbUri: "mongodb://localhost:27017/Expence_tacker",
+  dbUri: "mongodb://127.0.0.1:27017/Expence_tacker",
   frontAppVerif: "http://localhost:4200/user/verifemail/",
   jwtKey: "okjdt-&;qw5^pr",
 };
